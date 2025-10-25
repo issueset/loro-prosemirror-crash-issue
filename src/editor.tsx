bc4873b2a2547c1a399eb9d6a9558e8cf6e4ba79
ocavue <ocavue@gmail.com>
2025-10-26 01:51:24 +1100

    fix

--- src/editor.tsx
+++ src/editor.tsx
@@ -1,6 +1,6 @@
 import { LoroDoc, type AwarenessListener } from 'loro-crdt'
 import { CursorAwareness, type LoroDocType } from 'loro-prosemirror'
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 
 import EditorComponent from './editor-component'
 
